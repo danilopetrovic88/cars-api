6430a91e34a6c254643b5fbf84ddb49f02f7c83d
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-07-05 18:51:49 +0200

    zadatak 1

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Car;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
+ */
+class CarFactory extends Factory
+{
+    protected $model = Car::class;
+    private const BRANDS = [
+        'mercedes',
+        'opel',
+        'peugeot',
+        'audi',
+    ];
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
