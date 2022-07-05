2b2c6e06b44199291f53d77d56f155c0e253c9f5
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-07-05 19:47:44 +0200

    zadatak 4 treba zavrsiti parametar page

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -14,9 +14,15 @@ class CarController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $cars = Car::all();
+        $per_page = $request->query('per_page', 3);
+
+        $page = $request->query('page');
+
+        $query = Car::query();
+
+        $cars = $query->paginate($per_page);
 
         return response()->json($cars);
     }
